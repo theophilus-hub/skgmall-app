4e47975838898edb5efef1ab7f2eaf6b315e17a7
BsoftLimited <okelekelenobel@gmail.com>
2024-12-30 15:59:49 +0000

    still working on it

--- context/GlobalProvider.ts
+++ context/GlobalProvider.ts
@@ -1,12 +1,13 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import { getUser } from "../lib/supabase";
+import React = require("react");
 
 
 const GlobalContext = createContext();
 
 export const useGlobalContext = () => useContext(GlobalContext);
 
-const GlobalProvider = ({ children }) => {
+const GlobalProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [user, setUser] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
